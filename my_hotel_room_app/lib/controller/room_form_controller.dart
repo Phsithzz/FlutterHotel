7b95493d7301b15277fe1b2680cd759baba17f63
Phsithzz <phoosith2548@gmail.com>
2026-02-27 16:02:37 +0700

    create isRent Controller

--- my_hotel_room_app/lib/controller/room_form_controller.dart
+++ my_hotel_room_app/lib/controller/room_form_controller.dart
@@ -15,6 +15,22 @@ class RoomFormController extends GetxController {
 
   Future<void> saveRoom(List roomId) async {
     try {
+      for (int i = 0; i < roomId.length; i++) {
+        bool isRentCheck = await isRent(
+          roomId[i],
+          AppUnity.dateFormat(date: dateRange.value.start),
+        );
+
+        if (isRentCheck == false) {
+          AppUnity.myShowSnackBar(
+            context: Get.context!,
+            text: "ไม่สามารถจองวันนี้ได้",
+            typeDialog: TypeDialog.error,
+          );
+          return;
+        }
+      }
+
       await HelperApi().httpPost(
         path: "/roomRent/rent",
         data: jsonEncode({
@@ -31,12 +47,34 @@ class RoomFormController extends GetxController {
       );
       Get.back(result: true);
     } catch (err) {
-      print(err);
       AppUnity.myShowSnackBar(
         context: Get.context!,
         text: err.toString(),
         typeDialog: TypeDialog.error,
       );
+    }
+  }
+
+  Future<bool> isRent(int roomId, String data) async {
+    try {
+      var body = await HelperApi().httpPost(
+        path: "/roomRent/isRent",
+        data: jsonEncode({"roomId": roomId, "checkinDate": "$data 00:00:00"}),
+      );
+
+      List listcheckroom = body["result"];
+      if (listcheckroom.isNotEmpty) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (err) {
+      AppUnity.myShowSnackBar(
+        context: Get.context!,
+        text: err.toString(),
+        typeDialog: TypeDialog.error,
+      );
+      rethrow;
     }
   }
 }
